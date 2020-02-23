16c6ec8d9cb6ce62c1378b40710fc166ac577b19
Henry Groover <henry.groover@gmail.com>
2020-02-23 03:29:37 -0600

    1.02: Minor enhancements include: * Added comment block with path to tour explanation (which speed applies) * Handle kml argument with extension or without * Add starting distance to segment description

--- tour-splitter.py
+++ tour-splitter.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 # Tour splitter - take a Google Maps tour in kml format and split into
-# multiple pieces so long tours can be rendered using Google Earh Pro
+# multiple pieces so long tours can be rendered using Google Earth Pro
 # MovieMaker (which tends to crash during really long tours).
+# A path is converted into a tour by highlighting the path object
+# then using the "Play tour" icon at the bottom of my places. Save
+# on the tour playback bar prompts for a name and saves the path as
+# a tour. The Options / Touring / When creating a tour from a line / Speed
+# in effect at the time of saving the path as a tour is what determines
+# the speed of the tour. There is no way to change the speed of the tour
+# later. Speed is in units under Options / 3D view / Units (either mph or kph).
 
 import xml.etree.ElementTree as ET
 import math
 import sys
+import os
 from math import radians, cos, sin, asin, sqrt
 
 # Return haversine distance in miles
@@ -23,18 +31,32 @@ def haversine(lat1, lon1, lat2, lon2):
 
       return R * c
 
-version = '1.01'
-maxtime = 900
+version = '1.02'
+# 10 minutes; at 240mph, about 24m
+maxtime = 600
+# 25 miles; at 250mph, about 10m
+maxdist = 25
+splitByTime = 0
+splitByDistance = 1
 if len(sys.argv) < 2:
    print 'Syntax: tour-splitter.py tourfile [maxtime]'
-   print '(do not include kml in tourfile name)'
    print 'maxtime is in seconds, default is 900 (15m)'
    sys.exit(1)
 basename = sys.argv[1]
+(base_base, base_ext) = os.path.splitext(basename)
+if base_ext != '':
+   basename = base_base
+   print 'Using basename', basename
+if not os.path.isfile(basename + '.kml'):
+   print 'Cannot read', basename + '.kml'
+   sys.exit(1)
 if len(sys.argv) > 2:
    maxtime = float(sys.argv[2])
- 
-print 'tour-splitter {} - parsing {}.kml, maximum time per segment = {}s'.format(version, basename, maxtime)
+
+if splitByTime > 0:
+   print 'tour-splitter {} - parsing {}.kml, maximum time per segment = {}s'.format(version, basename, maxtime)
+else:
+   print 'tour-splitter {} - parsing {}.kml, maximum distance per segment = {}m'.format(version, basename, maxdist)
 
 tree = ET.parse(basename + '.kml')
 root = tree.getroot()
@@ -107,14 +129,17 @@ else:
 postamble = "</ns1:Playlist>\r\n</ns1:Tour>\r\n</ns0:kml>\r\n"
 
 startTime = 0.0
+startDist = 0.0
 lastFlyTo = ''
+prevlat = 0
+prevlon = 0
 for x in range(1,chunkRounded+1):
   filename = "{2}-{0}of{1}.kml".format(x, chunkRounded, basename)
   print 'Writing', filename, 'starting from', startTime
   with open(filename, 'wb') as f:
      f.write(preamble)
      f.write("<ns0:name>{0} ({1} of {2})</ns0:name>\r\n".format(name, x, chunkRounded))
-     f.write("<ns0:description>{0} starting {1}m {2}s</ns0:description>\r\n".format(description, int(startTime / 60), startTime - 60 * int(startTime / 60)))
+     f.write("<ns0:description>{0} starting {1}m {2}s miles {3}</ns0:description>\r\n".format(description, int(startTime / 60), startTime - 60 * int(startTime / 60), startDist))
      f.write("<ns1:Playlist>\r\n")
      # Skip leading elements
      chunkStartTime = 0.0
@@ -125,6 +150,14 @@ for x in range(1,chunkRounded+1):
            duration = float(child.find('{http://www.google.com/kml/ext/2.2}duration').text)
            #if x == 1:
            #   print 'Fly to:', duration, child.find('{http://www.google.com/kml/ext/2.2}duration').text
+           lookat = child.find('{http://www.opengis.net/kml/2.2}LookAt')
+           lat = float(lookat.find('{http://www.opengis.net/kml/2.2}latitude').text)
+           lon = float(lookat.find('{http://www.opengis.net/kml/2.2}longitude').text)
+           distance = 0
+           if prevlat != 0:
+              distance = haversine( prevlat, prevlon, lat, lon )
+           prevlat = lat
+           prevlon = lon
         else:
            print 'Warning: discarding ' + ET.tostring(child)
            continue
@@ -134,13 +167,14 @@ for x in range(1,chunkRounded+1):
            chunkStartTime = chunkStartTime + duration
            continue
         if chunkIndex == 0:
-           print 'Writing chunk {0} starting at {1} duration {2}'.format(x, startTime, duration)
+           print 'Writing chunk {0} starting at {1}s duration {2} mile {3}'.format(x, startTime, duration, startDist)
            # Replicate the last record of the previous chunk with ns0:FlyToMode removed
            if x > 1:
               f.write(lastFlyTo.replace('<ns0:flyToMode>smooth</ns0:flyToMode>', ''))
         lastFlyTo = ET.tostring(child)
         f.write(lastFlyTo)
         startTime = startTime + duration
+        startDist = startDist + distance
         chunkStartTime = chunkStartTime + duration
         chunkElapsed = chunkElapsed + duration
         chunkIndex = chunkIndex + 1
